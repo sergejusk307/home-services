292b1e8c74338b05606f76dffe8d34b7c05105e9
Sergejus <13186503+sergejusk307@users.noreply.github.com>
2024-11-07 20:46:54 +0200

    clear

--- home-services-api/src/exceptions/HttpException.js
+++
@@ -1,9 +0,0 @@
-class HttpException extends Error {
-  constructor(statusCode, message, error = {}) {
-    super(message);
-    this.statusCode = statusCode;
-    this.error = error;
-  }
-}
-
-export default HttpException;
